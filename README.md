84b6cc55f450dfcdbac4074c40f33bc8f11d866a
Matt Ngaw <mngaw@andrew.cmu.edu>
2022-07-12 16:25:57 -0400

    Initial commit; initial interface

---
+++ README.md
@@ -0,0 +1,11 @@
+<div align="center">
+    <br/>
+    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/50b370ee-c493-4df7-a9b4-f1b0c11f3dec/d5zvzw0-ae628290-d827-468d-a1e5-2b8a41414673.png/v1/fill/w_894,h_894,q_70,strp/om_nom_monster_by_frostbiteb82_d5zvzw0-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MjAwMCIsInBhdGgiOiJcL2ZcLzUwYjM3MGVlLWM0OTMtNGRmNy1hOWI0LWYxYjBjMTFmM2RlY1wvZDV6dnp3MC1hZTYyODI5MC1kODI3LTQ2OGQtYTFlNS0yYjhhNDE0MTQ2NzMucG5nIiwid2lkdGgiOiI8PTIwMDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.mOzcqvjOcJmdzYq9-MPAk9T8w-wUWXIzi0Q_sRohkP8" alt="Rust Monke Logo" width="256"/>
+    <p><em>poly om nom nom</em></p>
+</div>
+
+# polynom
+
+A C library for creating and working with polynomials.
+
+It is currently a work in progress.
---
+++ src/polynom.h
@@ -0,0 +1,49 @@
+#ifndef __POLYNOM_H__
+#define __POLYNOM_H__
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct polynom_header *Polynom;
+
+/* Create and destroy */
+Polynom polynom_new();
+Polynom polynom_copy(Polynom p);
+Polynom polynom_zero();
+Polynom polynom_one();
+Polynom polynom_x();
+Polynom polynom_x_to_the(size_t k);
+void polynom_destroy(Polynom p);
+
+/* Coefficients */
+double polynom_coeff_get(Polynom p, size_t i);
+void polynom_coeff_set(Polynom p, size_t i, double x);
+
+/* Degree */
+size_t polynom_degree_get(Polynom p);
+int polynom_degree_compare(Polynom p);
+
+/* Equality and order */
+bool polynom_equal(Polynom p1, Polynom p2);
+int polynom_compare(Polynom p1, Polynom p2);
+
+/* Arithmetic */
+double polynom_evaluate(Polynom p, double x);
+void polynom_add(Polynom q, Polynom p1, Polynom p2);
+void polynom_subtract(Polynom q, Polynom p1, Polynom p2);
+void polynom_multiply_scalar(Polynom q, Polynom p1, double x);
+void polynom_multiply_vector(Polynom q, Polynom p1, Polynom p2);
+void polynom_divide_scalar(Polynom q, Polynom p1, Polynom p2);
+void polynom_divide_vector(Polynom q, Polynom p1, Polynom p2);
+void polynom_power(Polynom q, Polynom p, int x);
+void polynom_scale(Polynom q, size_t k);
+void polynom_reduce(Polynom q, Polynom p);
+
+/* Calculus */
+void polynom_derive(Polynom q, Polynom p);
+void polynom_integrate(Polynom q, Polynom p);
+
+/* Conversion */
+void polynom_to_string(Polynom p, char *buf);
+void polynom_to_array(Polynom p, double *buf);
+Polynom polynom_from_array(double *buf);
