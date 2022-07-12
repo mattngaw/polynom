b586985614f773d3dc2373ab8e6bb3051ddc52b4
Matt Ngaw <mngaw@andrew.cmu.edu>
2022-07-12 19:52:13 -0400

    Implement coefficient methods

--- src/polynom.c
+++ src/polynom.c
@@ -1,6 +1,13 @@
+#include <assert.h>
 #include <string.h>
 
 #include "polynom.h"
+
+#ifdef DEBUG
+#define debug_assert(expr) assert(expr)
+#else
+#define debug_assert(expr) do {} while(false)
+#endif
 
 struct polynom_header {
     double *terms;
@@ -17,6 +24,8 @@ struct polynom_header {
 /******************
  * Implementation *
  ******************/
+
+/* Create and destroy */
 
 /**
  * @brief Initializes a polynomial
@@ -81,7 +90,7 @@ Polynom polynom_x() {
     Polynom q = polynom_new();
     q->degree = 1;
     q->terms_length = 2;
-    q->terms = calloc(sizeof(double) * q->terms_length);
+    q->terms = calloc(sizeof(double), q->terms_length);
     q->terms[1] = 1.0;
     return q;
 }
@@ -96,7 +105,7 @@ Polynom polynom_x_to_the(size_t k) {
     Polynom q = polynom_new();
     q->degree = k;
     q->terms_length = k + 1;
-    q->terms = calloc(sizeof(double) * q->terms_length);
+    q->terms = calloc(sizeof(double), q->terms_length);
     q->terms[k] = 1.0;
     return q;
 }
@@ -116,11 +125,45 @@ void polynom_destroy(Polynom p) {
 }
 
 /* Coefficients */
-double polynom_coeff_get(Polynom p, size_t i);
-void polynom_coeff_set(Polynom p, size_t i, double x);
+
+/**
+ * @brief Gets the ith coefficient of a polynomial
+ * 
+ * @param[in] p The polynomial
+ * @param[in] i The coefficient index
+ * 
+ * @return The coefficient
+ */
+double polynom_coeff_get(Polynom p, size_t i) {
+    debug_assert(i < p->terms_length);
+    if (i > p->degree) return 0.0;
+    return p->terms[i];
+}
+
+/**
+ * @brief Sets the ith coefficient of a polynomial
+ * 
+ * @param[in] p The polynomial
+ * @param[in] i The coefficient index
+ * @param[in] a The new coefficient value 
+ */
+void polynom_coeff_set(Polynom p, size_t i, double a) {
+    if (i >= p->terms_length) {
+        double *temp = p->terms;
+        p->terms = calloc(sizeof(double), i+1);
+        memcpy(p->terms, temp, sizeof(double) * p->terms_length);
+    }
+    p->terms[i] = a;
+    p->degree = i;
+    return;
+}
+
+void polynom_to_string(Polynom p, char *buf);
+void polynom_to_array(Polynom p, double *buf);
+Polynom polynom_from_array(double *buf);
 
 /* Degree */
-size_t polynom_degree_get(Polynom p);
+size_t polynom_degree(Polynom p);
 int polynom_degree_compare(Polynom p);
 
 /* Equality and order */
@@ -131,19 +174,14 @@ int polynom_compare(Polynom p1, Polynom p2);
 double polynom_evaluate(Polynom p, double x);
 void polynom_add(Polynom q, Polynom p1, Polynom p2);
 void polynom_subtract(Polynom q, Polynom p1, Polynom p2);
-void polynom_multiply_scalar(Polynom q, Polynom p1, double x);
+void polynom_multiply_scalar(Polynom q, Polynom p1, double c);
 void polynom_multiply_vector(Polynom q, Polynom p1, Polynom p2);
 void polynom_divide_scalar(Polynom q, Polynom p1, Polynom p2);
 void polynom_divide_vector(Polynom q, Polynom p1, Polynom p2);
-void polynom_power(Polynom q, Polynom p, int x);
+void polynom_power(Polynom q, Polynom p, int k);
 void polynom_scale(Polynom q, size_t k);
 void polynom_reduce(Polynom q, Polynom p);
 
 /* Calculus */
 void polynom_derive(Polynom q, Polynom p);
 void polynom_integrate(Polynom q, Polynom p);
-
-/* Conversion */
-void polynom_to_string(Polynom p, char *buf);
-void polynom_to_array(Polynom p, double *buf);
-Polynom polynom_from_array(double *buf);
--- src/polynom.h
+++ src/polynom.h
@@ -17,10 +17,13 @@ void polynom_destroy(Polynom p);
 
 /* Coefficients */
 double polynom_coeff_get(Polynom p, size_t i);
-void polynom_coeff_set(Polynom p, size_t i, double x);
+void polynom_coeff_set(Polynom p, size_t i, double a);
+void polynom_to_string(Polynom p, char *buf);
+void polynom_to_array(Polynom p, double *buf);
+Polynom polynom_from_array(double *buf);
 
 /* Degree */
-size_t polynom_degree_get(Polynom p);
+size_t polynom_degree(Polynom p);
 int polynom_degree_compare(Polynom p);
 
 /* Equality and order */
@@ -31,19 +34,14 @@ int polynom_compare(Polynom p1, Polynom p2);
 double polynom_evaluate(Polynom p, double x);
 void polynom_add(Polynom q, Polynom p1, Polynom p2);
 void polynom_subtract(Polynom q, Polynom p1, Polynom p2);
-void polynom_multiply_scalar(Polynom q, Polynom p1, double x);
+void polynom_multiply_scalar(Polynom q, Polynom p1, double c);
 void polynom_multiply_vector(Polynom q, Polynom p1, Polynom p2);
 void polynom_divide_scalar(Polynom q, Polynom p1, Polynom p2);
 void polynom_divide_vector(Polynom q, Polynom p1, Polynom p2);
-void polynom_power(Polynom q, Polynom p, int x);
+void polynom_power(Polynom q, Polynom p, int k);
 void polynom_scale(Polynom q, size_t k);
 void polynom_reduce(Polynom q, Polynom p);
 
 /* Calculus */
 void polynom_derive(Polynom q, Polynom p);
 void polynom_integrate(Polynom q, Polynom p);
-
-/* Conversion */
-void polynom_to_string(Polynom p, char *buf);
-void polynom_to_array(Polynom p, double *buf);
-Polynom polynom_from_array(double *buf);
